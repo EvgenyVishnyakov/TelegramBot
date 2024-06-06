88f75c46a6b076fbf2c9ae6e1025030e425c80c3
hellogirl 91112 <ololo128666@gmail.com>
2024-06-07 00:43:50 +0500

    Начал разработку

--- IRON_PROGRAMMER_BOT/IRON_PROGRAMMER_BOT_ConsoleApp/Program.cs
+++ IRON_PROGRAMMER_BOT/IRON_PROGRAMMER_BOT_ConsoleApp/Program.cs
@@ -1,10 +1,25 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!!");
+        var telegramBotClient = new TelegramBotClient("7244274831:AAEKs7B6FHzqLBwWYc6nxv6xad6tUFImgnU");
+        telegramBotClient.StartReceiving(updateHandler:HandleUpdate,pollingErrorHandler: HandlePoolingError);
+    }
+
+    private static async Task HandlePoolingError(ITelegramBotClient client, Exception exception, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static async Task HandleUpdate(ITelegramBotClient client, Update update, CancellationToken token)
+    {
+        throw new NotImplementedException();
     }
 }
 
