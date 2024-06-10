74fa7995a10851e49a3e527930e5a43573518d98
hellogirl 91112 <ololo128666@gmail.com>
2024-06-10 23:25:10 +0500

    реализация ответа

--- IRON_PROGRAMMER_BOT/IRON_PROGRAMMER_BOT_ConsoleApp/Program.cs
+++ IRON_PROGRAMMER_BOT/IRON_PROGRAMMER_BOT_ConsoleApp/Program.cs
@@ -6,22 +6,32 @@ using Telegram.Bot.Types;
 
 class Program
 {
-      static void Main(string[] args)
+      static async Task Main(string[] args)
     {
         var telegramBotClient = new TelegramBotClient("7244274831:AAEKs7B6FHzqLBwWYc6nxv6xad6tUFImgnU");
+        var user = await telegramBotClient.GetMeAsync();
         telegramBotClient.StartReceiving(updateHandler: HandleUpdate, pollingErrorHandler: HandlePoolingError);
+        
+        Console.WriteLine($"Начали слушать апдеты с {user.Username}");
+        Console.ReadLine();
+    }
+
+    
+
+    private static async Task HandleUpdate(ITelegramBotClient client, Update update, CancellationToken token)
+    {
+        if (update.Message?.Text!=null)
+        {
+            var chatId = update.Message.Chat.Id; //получаем инфу с update
+            var text = update.Message.Text;//update  у него берем 
+            await client.SendTextMessageAsync(chatId: chatId, $"Бот ответит вам: \t {text}");
+        }
     }
 
     private static async Task HandlePoolingError(ITelegramBotClient client, Exception exception, CancellationToken token)
     {
-        throw new NotImplementedException();
+        Console.WriteLine(exception.Message);
     }
-
-    private static async Task HandleUpdate(ITelegramBotClient client, Update update, CancellationToken token)
-    {
-        throw new NotImplementedException();
-    }
-
 
 }
 
