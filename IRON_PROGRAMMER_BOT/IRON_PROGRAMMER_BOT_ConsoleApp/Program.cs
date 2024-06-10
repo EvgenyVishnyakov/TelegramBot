2cc87f10dafd3b4a6cb2ac442065805de6e126a1
hellogirl 91112 <ololo128666@gmail.com>
2024-06-10 23:58:24 +0500

    Сообщение с цитатой

--- IRON_PROGRAMMER_BOT/IRON_PROGRAMMER_BOT_ConsoleApp/Program.cs
+++ IRON_PROGRAMMER_BOT/IRON_PROGRAMMER_BOT_ConsoleApp/Program.cs
@@ -6,25 +6,37 @@ using Telegram.Bot.Types;
 
 class Program
 {
-      static async Task Main(string[] args)
+    static async Task Main(string[] args)
     {
         var telegramBotClient = new TelegramBotClient("7244274831:AAEKs7B6FHzqLBwWYc6nxv6xad6tUFImgnU");
         var user = await telegramBotClient.GetMeAsync();
-        telegramBotClient.StartReceiving(updateHandler: HandleUpdate, pollingErrorHandler: HandlePoolingError);
-        
+        telegramBotClient.StartReceiving(updateHandler: HandleUpdate, pollingErrorHandler: HandlePoolingError);//ddos каждые 50 секунд
+
         Console.WriteLine($"Начали слушать апдеты с {user.Username}");
         Console.ReadLine();
     }
 
-    
+
 
     private static async Task HandleUpdate(ITelegramBotClient client, Update update, CancellationToken token)
     {
-        if (update.Message?.Text!=null)
+        if (update.Message?.Text != null)
         {
-            var chatId = update.Message.Chat.Id; //получаем инфу с update
-            var text = update.Message.Text;//update  у него берем 
-            await client.SendTextMessageAsync(chatId: chatId, $"Бот ответит вам: \t {text}");
+            var chatId = update.Message.Chat.Id;
+            var text = update.Message.Text;
+            var messageId = update.Message.MessageId;
+
+            //просто сообщение с текстом
+            await client.SendTextMessageAsync(
+                chatId: chatId,
+                text: $"Бот ответит вам: \t {text}");
+
+            //сообщение с цитатой
+            await client.SendTextMessageAsync(
+                chatId: chatId,
+                text: $"Бот ответит вам: \t {text}",
+                replyToMessageId: messageId);
+
         }
     }
 
