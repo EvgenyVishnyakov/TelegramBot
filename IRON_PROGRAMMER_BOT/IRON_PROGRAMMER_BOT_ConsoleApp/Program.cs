5d55c2e07d7363176823292ba69b18cb60946819
Evgeny Vishnyakov <vis-evgenij@yandex.ru>
2025-02-14 16:29:17 +0300

    исправлены замечания

--- IRON_PROGRAMMER_BOT/IRON_PROGRAMMER_BOT_ConsoleApp/Program.cs
+++ IRON_PROGRAMMER_BOT/IRON_PROGRAMMER_BOT_ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
@@ -25,14 +26,15 @@ class Program
 
         if (update.Message?.Text != null)
         {
+            long chatId;
+            string text;
+            GetDataBYMessage(update, out chatId, out text);
             if (update.Message.Text.StartsWith("/buttons"))
             {
                 var data = update.Message.Text.Split();
                 if (data.Length == 3)
                 {
-                    long chatId;
-                    string text;
-                    GetDataBYMessage(update, out chatId, out text);
+
                     if (int.TryParse(data[1], out var countRows) && int.TryParse(data[2], out var countColumns))
                         await GetReplyUserButtons(client, chatId, text, countRows, countColumns);
                     else
@@ -44,10 +46,6 @@ class Program
                 var data = update.Message.Text.Split();
                 if (data.Length == 3)
                 {
-                    long chatId;
-                    string text;
-                    GetDataBYMessage(update, out chatId, out text);
-
                     if (int.TryParse(data[1], out var countRows) && int.TryParse(data[2], out var countColumns))
                     {
                         var buttons = GetInlineButtons(countRows, countColumns);
@@ -58,46 +56,43 @@ class Program
                         await client.SendTextMessageAsync(chatId: chatId, text: $"Вы прислали неверные числовые данные");
                 }
             }
-
-
-
-            if (update.Message?.Text != null)
+            if (update.Message.Text.StartsWith("/photo"))
             {
                 var data = update.Message.Text.Split();
-                var chatId = update.Message.Chat.Id;
-                if (data[0] == "/photo")
+                if (data.Length == 2)
                 {
-                    if (data.Length == 2)
+                    var pathPhoto = data[1];
+                    await client.SendPhotoAsync(
+                        chatId: chatId,
+                        InputFile.FromUri(pathPhoto),
+                        caption: "Вот ваша фотка"
+                        );
+                }
+                else
+                {
+                    var photo = GetRandomPhoto();
+                    using (var file = new FileStream($@"Images/{photo}", FileMode.Open, FileAccess.Read))
                     {
-                        var pathPhoto = data[1];
-
-                        var text = update.Message.Text;
-                        var messageId = update.Message.MessageId;
-
                         await client.SendPhotoAsync(
                             chatId: chatId,
-                            InputFile.FromUri(pathPhoto),
-                            caption: "Вот ваша фотка"
+                            InputFile.FromStream(file),
+                            caption: photo
                             );
-                    }
-                    else
-                    {
-                        var listPhoto = GetListPhoto();
-                        var random = new Random();
-                        var photoIndex = random.Next(listPhoto.Length);
-
-                        using (var file = new FileStream($@"Images\{listPhoto[photoIndex]}", FileMode.Open, FileAccess.Read))
-                        {
-                            await client.SendPhotoAsync(
-                                chatId: chatId,
-                                InputFile.FromStream(file),
-                                caption: listPhoto[photoIndex]
-                                );
-                        }
                     }
                 }
             }
         }
+    }
+
+    private static string GetRandomPhoto()
+    {
+        var listPhoto = GetListPhoto();
+        var random = new Random();
+        var photoIndex = random.Next(listPhoto.Length);
+
+        return listPhoto[photoIndex];
+    }
+
 
     private static string[] GetListPhoto()
     {
@@ -107,6 +102,66 @@ class Program
                     "Фото3.jpg",
                     "Фото4.png"
                 };
+    }
+
+    private static List<List<InlineKeyboardButton>> GetInlineButtons(int countRows, int countColumns)
+    {
+        var buttons = new List<List<InlineKeyboardButton>>();
+
+        var counterButtons = 1;
+
+        for (int i = 0; i < countRows; i++)
+        {
+            var row = new List<InlineKeyboardButton>();
+            for (int j = 0; j < countColumns; j++)
+            {
+                row.Add(new InlineKeyboardButton(counterButtons.ToString())
+                {
+                    CallbackData = "Успешно"
+                }
+                );
+                counterButtons++;
+            }
+            buttons.Add(row);
+        }
+
+        return buttons;
+    }
+
+    private static async Task GetReplyUserButtons(ITelegramBotClient client, long chatId, string text, int countRows, int countColumns)
+    {
+        var buttons = GetReplayButtons(countRows, countColumns);
+        await client.SendTextMessageAsync(chatId: chatId, text: $"Вы прислали: \n {text}",
+            replyMarkup: new ReplyKeyboardMarkup(buttons)
+            {
+                ResizeKeyboard = true
+            });
+    }
+
+    private static void GetDataBYMessage(Update update, out long chatId, out string text)
+    {
+        chatId = update.Message.Chat.Id;
+        text = update.Message.Text;
+        var messageId = update.Message.MessageId;
+    }
+
+    private static List<List<KeyboardButton>> GetReplayButtons(int countRows, int countColumns)
+    {
+        var buttons = new List<List<KeyboardButton>>();
+
+        var buttonsCounter = 1;
+
+        for (int i = 0; i < countRows; i++)
+        {
+            var row = new List<KeyboardButton>();
+            for (int j = 0; j < countColumns; j++)
+            {
+                row.Add(new KeyboardButton(buttonsCounter.ToString()));
+                buttonsCounter++;
+            }
+            buttons.Add(row);
+        }
+        return buttons;
     }
 
     private static async Task HandlePollingError(ITelegramBotClient client, Exception exception, CancellationToken token)
