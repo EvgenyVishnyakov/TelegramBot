d9ba4c9bb8fcf2cb9f9ed309366b52f781e31077
Evgeny Vishnyakov <vis-evgenij@yandex.ru>
2025-02-14 15:17:03 +0300

    +

--- IRON_PROGRAMMER_BOT/IRON_PROGRAMMER_BOT_ConsoleApp/Program.cs
+++ IRON_PROGRAMMER_BOT/IRON_PROGRAMMER_BOT_ConsoleApp/Program.cs
@@ -10,7 +10,8 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        var telegramBotClient = new TelegramBotClient("7239844885:AAHDJcHRUexZj1FtxlTPlI7-XoeFN7yByxg");
+        string paramBot = Environment.GetEnvironmentVariable("paramBot")!;
+        var telegramBotClient = new TelegramBotClient(paramBot);
 
         var user = await telegramBotClient.GetMeAsync();
         Console.WriteLine($"Начали слушать updates {user.Username}");
@@ -24,46 +25,56 @@ class Program
     {
         if (update.Message?.Text != null)
         {
-            var data = update.Message.Text.Split();
-            if (data[0] == "/buttons" && data.Length == 3)
+            if (update.Message.Text.StartsWith("/buttons"))
             {
-                var first_size = int.Parse(data[1]);
-                var second_size = int.Parse(data[2]);
-
-                var buttons = GetReplayButtons(first_size, second_size);
-
-                var chatId = update.Message.Chat.Id;
-                var text = update.Message.Text;
-                var messageId = update.Message.MessageId;
-
-                await client.SendTextMessageAsync(chatId: chatId, text: $"Вы прислали: \n {text}",
-                    replyMarkup: new ReplyKeyboardMarkup(buttons)
-
-                    {
-                        ResizeKeyboard = true
-                    });
-
+                var data = update.Message.Text.Split();
+                if (data.Length == 3)
+                {
+                    long chatId;
+                    string text;
+                    GetDataBYMessage(update, out chatId, out text);
+                    if (int.TryParse(data[1], out var countRows) && int.TryParse(data[2], out var countColumns))
+                        await GetReplyUserButtons(client, chatId, text, countRows, countColumns);
+                    else
+                        await client.SendTextMessageAsync(chatId: chatId, text: $"Вы прислали неверные числовые данные");
+                }
             }
         }
+    }
+
+    private static async Task GetReplyUserButtons(ITelegramBotClient client, long chatId, string text, int countRows, int countColumns)
+    {
+        var buttons = GetReplayButtons(countRows, countColumns);
+        await client.SendTextMessageAsync(chatId: chatId, text: $"Вы прислали: \n {text}",
+            replyMarkup: new ReplyKeyboardMarkup(buttons)
+            {
+                ResizeKeyboard = true
+            });
+    }
+
+    private static void GetDataBYMessage(Update update, out long chatId, out string text)
+    {
+        chatId = update.Message.Chat.Id;
+        text = update.Message.Text;
+        var messageId = update.Message.MessageId;
     }
 
     private static List<List<KeyboardButton>> GetReplayButtons(int first_size, int second_size)
     {
         var buttons = new List<List<KeyboardButton>>();
 
-        var counterButtons = 1;
+        var buttonsCounter = 1;
 
         for (int i = 0; i < first_size; i++)
         {
             var row = new List<KeyboardButton>();
             for (int j = 0; j < second_size; j++)
             {
-                row.Add(new KeyboardButton(counterButtons.ToString()));
-                counterButtons++;
+                row.Add(new KeyboardButton(buttonsCounter.ToString()));
+                buttonsCounter++;
             }
             buttons.Add(row);
         }
-
         return buttons;
     }
 
