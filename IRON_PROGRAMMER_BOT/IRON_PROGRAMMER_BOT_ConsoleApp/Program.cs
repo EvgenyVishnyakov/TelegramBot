f7272590cff0ed8e93b564f0d4b43150df9260ef
Evgeny Vishnyakov <vis-evgenij@yandex.ru>
2025-02-14 16:04:00 +0300

    +

--- IRON_PROGRAMMER_BOT/IRON_PROGRAMMER_BOT_ConsoleApp/Program.cs
+++ IRON_PROGRAMMER_BOT/IRON_PROGRAMMER_BOT_ConsoleApp/Program.cs
@@ -4,6 +4,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
 
 class Program
 {
@@ -22,47 +23,81 @@ class Program
     private static async Task HandleUpdate(ITelegramBotClient client, Update update, CancellationToken token)
     {
 
-
-
-
-
         if (update.Message?.Text != null)
         {
-            var data = update.Message.Text.Split();
-            var chatId = update.Message.Chat.Id;
-            if (data[0] == "/photo")
+            if (update.Message.Text.StartsWith("/buttons"))
             {
-                if (data.Length == 2)
+                var data = update.Message.Text.Split();
+                if (data.Length == 3)
                 {
-                    var pathPhoto = data[1];
-
-                    var text = update.Message.Text;
-                    var messageId = update.Message.MessageId;
-
-                    await client.SendPhotoAsync(
-                        chatId: chatId,
-                        InputFile.FromUri(pathPhoto),
-                        caption: "Вот ваша фотка"
-                        );
+                    long chatId;
+                    string text;
+                    GetDataBYMessage(update, out chatId, out text);
+                    if (int.TryParse(data[1], out var countRows) && int.TryParse(data[2], out var countColumns))
+                        await GetReplyUserButtons(client, chatId, text, countRows, countColumns);
+                    else
+                        await client.SendTextMessageAsync(chatId: chatId, text: $"Вы прислали неверные числовые данные");
                 }
-                else
+            }
+            if (update.Message.Text.StartsWith("/inline_buttons"))
+            {
+                var data = update.Message.Text.Split();
+                if (data.Length == 3)
                 {
-                    var listPhoto = GetListPhoto();
-                    var random = new Random();
-                    var photoIndex = random.Next(listPhoto.Length);
+                    long chatId;
+                    string text;
+                    GetDataBYMessage(update, out chatId, out text);
 
-                    using (var file = new FileStream($@"Images\{listPhoto[photoIndex]}", FileMode.Open, FileAccess.Read))
+                    if (int.TryParse(data[1], out var countRows) && int.TryParse(data[2], out var countColumns))
                     {
+                        var buttons = GetInlineButtons(countRows, countColumns);
+                        await client.SendTextMessageAsync(chatId: chatId, text: $"Вы прислали: \n {text}",
+                            replyMarkup: new InlineKeyboardMarkup(buttons));
+                    }
+                    else
+                        await client.SendTextMessageAsync(chatId: chatId, text: $"Вы прислали неверные числовые данные");
+                }
+            }
+
+
+
+            if (update.Message?.Text != null)
+            {
+                var data = update.Message.Text.Split();
+                var chatId = update.Message.Chat.Id;
+                if (data[0] == "/photo")
+                {
+                    if (data.Length == 2)
+                    {
+                        var pathPhoto = data[1];
+
+                        var text = update.Message.Text;
+                        var messageId = update.Message.MessageId;
+
                         await client.SendPhotoAsync(
                             chatId: chatId,
-                            InputFile.FromStream(file),
-                            caption: listPhoto[photoIndex]
+                            InputFile.FromUri(pathPhoto),
+                            caption: "Вот ваша фотка"
                             );
+                    }
+                    else
+                    {
+                        var listPhoto = GetListPhoto();
+                        var random = new Random();
+                        var photoIndex = random.Next(listPhoto.Length);
+
+                        using (var file = new FileStream($@"Images\{listPhoto[photoIndex]}", FileMode.Open, FileAccess.Read))
+                        {
+                            await client.SendPhotoAsync(
+                                chatId: chatId,
+                                InputFile.FromStream(file),
+                                caption: listPhoto[photoIndex]
+                                );
+                        }
                     }
                 }
             }
         }
-    }
 
     private static string[] GetListPhoto()
     {
